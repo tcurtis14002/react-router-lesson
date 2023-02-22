9ef03ea591fd5ea6b71a1b7b8b230bd89190a6c0
Tyler <willdev0110@gmail.com>
2023-02-21 19:58:36 -0700

    add forward and back butn functionality

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,19 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Footer() {
-
+  const navigate = useNavigate();
   // get the navigate function
 
   const goBack = () => {
     // imperatively redirect back
-    
-  }
+    navigate(-1);
+  };
 
   const goForward = () => {
     // imperatively redirect forward
-    
-  }
+    navigate(1);
+  };
 
   return (
     <footer>

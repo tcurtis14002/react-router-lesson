25545d203b435cee325a7c9cf4c4537f7d3e730c
Tyler <willdev0110@gmail.com>
2023-02-21 19:58:25 -0700

    add feature, cannot access profile if logged out

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, Navigate } from "react-router-dom";
 import {
   selectCurrentUser,
   selectIsLoggedIn,
@@ -11,7 +11,9 @@ export default function Profile() {
   const loggedIn = useSelector(selectIsLoggedIn);
 
   // use loggedIn to return a Navigate
-
+  if (!loggedIn) {
+    return <Navigate to="/sign-up" />;
+  }
   return (
     <main>
       <h1>{currentUser.username}</h1>
